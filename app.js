17279027bbe2ddab6743a88bd474a60b0dc2fd8f
binetouguey3s <binetouguey3s@gmail.com>
2026-06-04 12:39:02 +0000

    modification de app.js

--- app.js
+++ app.js
@@ -190,7 +190,7 @@ form.addEventListener('submit', async function(event) {
         return prompt
     // testme fin
 
-/// test pour forcer le modele         
+/// test pour forcer le modele    a vercel      
 export default async function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).json({ error: 'Method not allowed' });
   try {
